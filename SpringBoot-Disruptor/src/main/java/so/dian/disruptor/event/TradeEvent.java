0c3f5c7711529e328a66e1a1e4af20a365ec64b8
baizhang <baizhang@dian.so>
2019-02-22 15:48:05 +0800

    fix

---
+++ SpringBoot-Disruptor/src/main/java/so/dian/disruptor/event/TradeEvent.java
@@ -0,0 +1,22 @@
+/*
+ * Dian.so Inc.
+ * Copyright (c) 2016-2019 All Rights Reserved.
+ */
+package so.dian.disruptor.event;
+
+import lombok.Data;
+
+import java.util.Date;
+
+/**
+ * TODO
+ *
+ * @author ${baizhang}
+ * @version $Id: TradeEvent.java, v 0.1 2019-02-21 1:59 PM Exp $
+ */
+@Data
+public class TradeEvent {
+    private String tradeNo;
+    private int amount;
+    private Date tradeTime;
+}
