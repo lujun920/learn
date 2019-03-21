c90c01f01e42d40859eec942dfeec2ac8f15657f
baizhang <baizhang@dian.so>
2019-03-21 15:57:06 +0800

    add modules JavaBase

---
+++ JavaBase/src/main/java/so/learn/base/App.java
@@ -0,0 +1,13 @@
+package so.learn.base;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
